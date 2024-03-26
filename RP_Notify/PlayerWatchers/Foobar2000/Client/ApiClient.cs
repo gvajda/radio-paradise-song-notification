d91817fb755429c4fb8d73d563b66cca793f2b2e
Gergely Vajda <vajda.gergely@gmail.com>
2024-03-26 09:53:35 -0400

    Fix warning message

--- RP_Notify/PlayerWatchers/Foobar2000/Client/ApiClient.cs
+++ RP_Notify/PlayerWatchers/Foobar2000/Client/ApiClient.cs
@@ -210,7 +210,7 @@ namespace Foobar2000.RESTClient.Client
                 path, method, queryParams, postBody, headerParams, formParams, fileParams,
                 pathParams, contentType);
             InterceptRequest(request);
-            var response = await RestClient.ExecuteTaskAsync(request);
+            var response = await RestClient.ExecuteAsync(request);
             InterceptResponse(request, response);
             return (Object)response;
         }
